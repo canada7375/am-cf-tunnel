3784bec76106b3bb982b3c4392a954b2e1d75d33
canada7375 <49834480+canada7375@users.noreply.github.com>
2024-10-27 00:52:34 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -10,7 +10,7 @@ import { connect } from 'cloudflare:sockets';
 
 // Generate your own UUID using the following command in PowerShell:
 // Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '2928d83a-ca75-4d52-b437-fa78c7601378';
+let userID = '397e41f3-686b-4c58-a8f3-05e9c782a8b7';
 
 // Proxy IPs to choose from
 let proxyIPs = [
